a41967c10b8a7a950320e6514e70cc4336229652
Dmitry Kulchitskiy <dmitry.kulchitskiy@gmail.com>
2018-09-16 00:37:51 +0300

    integrate cell

--- viYarn/Story/view/StoryViewController.swift
+++ viYarn/Story/view/StoryViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class StoryViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
+class StoryViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    let cellID = "CellId"
     let colorBackground: UIColor = #colorLiteral(red: 0.09803921569, green: 0.1215686275, blue: 0.1568627451, alpha: 1)
+    
+    lazy var cellLayoutHeight:CGFloat = {
+        return self.view.bounds.height * 0.7
+    }()
+    lazy var cellLayoutWidth:CGFloat = {
+        return self.view.bounds.width * 0.9
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = colorBackground
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        collectionView?.register(StoryCollectionViewCell.self, forCellWithReuseIdentifier: StoryCollectionViewCell.cellID)
+        collectionView?.contentInset.top = (self.view.bounds.height - cellLayoutHeight) / 2
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,9 +32,7 @@ class StoryViewController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellWidth: CGFloat =  view.bounds.width * 0.9
-        let cellHeight: CGFloat =  view.bounds.height * 0.7
-        return CGSize(width: cellWidth, height: cellHeight)
+        return CGSize(width: cellLayoutWidth, height: cellLayoutHeight)
         
     }
     
@@ -36,10 +41,23 @@ class StoryViewController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath as IndexPath)
-        cell.backgroundColor = UIColor.white
-        return cell
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCollectionViewCell.cellID, for: indexPath as IndexPath) as? StoryCollectionViewCell {
+            return cell.configured()
+        }
+        return UICollectionViewCell()
+    }
+    
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = self.collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellHeight = cellLayoutHeight + layout.minimumLineSpacing
+        
+        var offset = targetContentOffset.pointee
+        let index = (offset.y + scrollView.contentInset.top) / cellHeight
+        let rounded = round(index)
+        
+        targetContentOffset.pointee = CGPoint(x: scrollView.contentInset.left, y: rounded * cellHeight - scrollView.contentInset.top)
     }
 
 }
+
 

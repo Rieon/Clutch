63ac22956475b80f82dc3a66e1c05af297c1fd95
Dmitry Kulchitskiy <dmitry.kulchitskiy@gmail.com>
2018-09-15 14:03:59 +0300

    refactoring

--- viYarn/Story/view/StoryViewController.swift
+++ viYarn/Story/view/StoryViewController.swift
@@ -8,37 +8,17 @@
 
 import UIKit
 
-class StoryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    
+class StoryViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
     let cellID = "CellId"
     var storyCollectionView: UICollectionView!
     let colorBackground: UIColor = #colorLiteral(red: 0.09803921569, green: 0.1215686275, blue: 0.1568627451, alpha: 1)
-    let screenSize = UIScreen.main.bounds
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let cellWidth: CGFloat = screenSize.width * 0.9
-        let cellHeight: CGFloat = screenSize.height * 0.7
         
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
-        
-        storyCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        storyCollectionView.delegate = self
-        storyCollectionView.dataSource = self
-        storyCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
-        storyCollectionView.backgroundColor = colorBackground
-        storyCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(storyCollectionView)
-        
-        storyCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-        storyCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
-        storyCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
-        storyCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        
+        collectionView?.backgroundColor = colorBackground
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
         
     }
 
@@ -46,14 +26,23 @@ class StoryViewController: UIViewController, UICollectionViewDataSource, UIColle
         super.didReceiveMemoryWarning()
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenSize = UIScreen.main.bounds
+        let cellWidth: CGFloat = screenSize.width * 0.9
+        let cellHeight: CGFloat = screenSize.height * 0.7
+        return CGSize(width: cellWidth, height: cellHeight)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath as IndexPath)
         cell.backgroundColor = UIColor.white
         return cell
     }
 
 }
+

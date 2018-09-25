63b37f5b31b6004536a6f163dd66f290be5043f6
Dmitry Kulchitskiy <dmitry.kulchitskiy@gmail.com>
2018-09-25 18:54:16 +0300

    fix p0

--- viYarn/Chat/model/EpisodeContent.swift
+++ viYarn/Chat/model/EpisodeContent.swift
@@ -20,38 +20,36 @@ extension EpisodeContent {
         guard let elements = try? doc.getAllElements() else { return nil }
         
         guard let episodeDescriptionElement = try? doc.select("p").first() else { return nil }
-        var episodeDescription = ""
-        if let elementDesc = episodeDescriptionElement {
-            if let descText = try? elementDesc.text(){
-                episodeDescription = descText
-            }
-        }
+        guard let elementDesc = episodeDescriptionElement else { return nil }
+        guard let episodeDescription  = try? elementDesc.text() else { return nil }
         
-        var currentAuthor: String = ""
-        var arrMessages = [(String, [String])]()
+    
+        var arrMessages = [(author: String, messages: [String])]()
         var currentAuthorMessages = [String]()
         for element in elements {
-
-            if element.tagName() == "b"{
-                guard let nameAuthor = try? element.text() else { return nil }
-                if currentAuthor.count == 0 {
-                    currentAuthor = nameAuthor
-                    continue
-                }
-                
-                if nameAuthor != currentAuthor {
-                    arrMessages.append((currentAuthor, currentAuthorMessages))
-                    currentAuthorMessages.removeAll()
-                }
-                currentAuthor = nameAuthor
-                continue
-            }
             
-            if element.tagName() == "span" {
+            switch element.tagName() {
+            case "b":
+                guard let nameAuthor = try? element.text() else { return nil }
+                if let last =  arrMessages.last {
+                    if nameAuthor != last.author{
+                        arrMessages.append((nameAuthor, []))
+                        currentAuthorMessages.removeAll()
+                    }
+                } else{
+                    arrMessages.append((nameAuthor, []))
+                }
+            case "span":
                 guard let message = try? element.text() else { return nil }
                 currentAuthorMessages.append(message)
+                arrMessages[arrMessages.count - 1].messages = currentAuthorMessages
+            default:
+                continue
+                
             }
         }
         self.init(chatMessages: arrMessages, episodeDescription: episodeDescription)
     }
+    
+    
 }

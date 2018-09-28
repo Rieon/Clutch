1e3d00b03621a404360fa0ff5e62f28598cc7b6e
Dmitry Kulchitskiy <dmitry.kulchitskiy@gmail.com>
2018-09-29 01:17:12 +0300

    main screen category

--- viYarn/Coordinator/AppCoordinator.swift
+++ viYarn/Coordinator/AppCoordinator.swift
@@ -18,30 +18,14 @@ class AppCoordinator: NSObject {
         self.window = window
     }
     func initFlow() {
-        let simpleViewController = UIViewController()
-        navController = UINavigationController(rootViewController: simpleViewController)
+        let mainScreenController = MainScreenTableViewController(didTapCategory: stoyScreenSelected)
+        navController = UINavigationController(rootViewController: mainScreenController)
         navController?.setNavigationBarHidden(false, animated: true)
         window.rootViewController = navController
-        loadCategories(didLoad: loadedCategories, failLoad: failLoad)
     }
-    func loadCategories(didLoad: @escaping([Category]) -> Void, failLoad: @escaping(Error) -> Void) {
-        APIClient.instance.request(typeRequest: .getCategories, success: {(loadCategories) in
-            guard let postsJson = loadCategories["categories"] as? [[String : Any]] else {
-                return failLoad(ParsingError.wrongData)
-            }
-            var postsArr = [Category]()
-            for postJson in postsJson {
-                guard let post = Category(json: postJson) else { return }
-                postsArr.append(post)
-            }
-            didLoad(postsArr)
-        }) { (error) in
-            failLoad(error)
-        }
-        
-    }
-    func loadedCategories(categories: [Category]) -> Void {
-        let storyController = StoryViewController(category: categories[1], didTapEpisode: episodeScreenSelected)
+    
+    func stoyScreenSelected(category: Category) -> Void {
+        let storyController = StoryViewController(category: category, didTapEpisode: episodeScreenSelected)
         navController?.setNavigationBarHidden(false, animated: true)
         navController?.pushViewController(storyController, animated: true)
     }
